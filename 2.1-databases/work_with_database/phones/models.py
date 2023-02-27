adfcb4d3088fd56fbea225a00d86361b4345f1cf
Vladimir Mosunov <vladimir_mosunov@mail.ru>
2023-02-27 20:30:10 +0300

    Hw_database-1

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(decimal_places=1, max_digits=9)
+    image = models.CharField(max_length=200)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length= 250, editable=False, unique=True)
+
+    def get_abs_url(self):
+        return reverse('phone', kwargs={'slug':self.slug})
+    
+    def __str__(self) :
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args,**kwargs)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,31 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort_p = request.GET.get('sort')
+    phone = Phone.objects.all() 
+
+    if sort_p == 'name':
+        phone = phone.order_by('name')
+        context = {'phones': phone}
+        return render(request, template, context)
+    elif sort_p == 'min_price':
+        phone = phone.order_by('price')
+        context = {'phones': phone}
+        return render(request, template, context)
+    elif sort_p == 'max_price':
+        phone = phone.order_by('-price')
+        context = {'phones': phone}
+        return render(request, template, context)
+
+
+    
+    phone = Phone.objects.all()
+    context = {'phones': phone}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
     return render(request, template, context)
