4df19c2fa6bd32c7c72e7c6836c4f6bfd33486bd
Vladimir Mosunov <vladimir_mosunov@mail.ru>
2023-03-01 18:09:26 +0300

    problem

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from books.models import Book
+from django.core.paginator import Paginator
 
+books = Book.objects.all()
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    context = {'books': books}
     return render(request, template, context)
+
+def book_view(request, pub_date):
+    paginator  = Paginator(books, 1)
+    p_num = Book.objects.get(pub_date=pub_date).id
+    page  = paginator.get_page(p_num)
+    template = 'books/books_list.html'
+    context = {'books': page,
+               'page': page,}
+    return render(request, template, context)
+
+
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from books.models import Book
 
-from books.views import books_view
+from books.views import books_view, book_view
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('books/', books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('books/<pub_date>/', book_view),
 ]
